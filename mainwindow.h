5d2c79484f49c5968e20712c07051224541f5e61
Seyyed Mostafa Mousavi <mostafa.mousavi.1379@gmail.com>
2026-04-30 17:05:40 +0330

    added connect button function in both mainwindow.h and .ui

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "serialcommunication.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -13,11 +14,33 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+signals:
+    void disconnect();
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+
+    void baudRates() ;
+    SerialCommunication *serialCom;
+    void onSerialConnected() ;
+    void onSerialError(const QString &msg);
+    void themeSelection();
+    void loadStyleSheet(const QString &path);
+    void coloruI();
+
+private slots:
+
+    void on_pbConnect_clicked();
+    void on_pbReloadPorts_clicked();
+    void on_pbPedConnect_clicked();
+    void on_pbPedTurnOnAzi_clicked();
+    void on_pb60_clicked();
+    void on_pb180_clicked();
+
+
 };
 #endif // MAINWINDOW_H
